bf4e623c64e8d8339f3173051358964301e0efa4
GuilherminDa16 <rm90611@estudante.fieb.edu.br>
2024-03-19 09:15:56 -0300

    busca

--- scripts.js
+++ scripts.js
@@ -23,17 +23,40 @@ var cidade = document.getElementById("cid");
 var estado = document.getElementById("est");
 
 
+
 function alertar (){
+
+
+    // buscar endereço pelo cep
+
+    const url = `https://viacep.com.br/ws/${cep.value}/json`;
+   
+    fetch (url)
+        .then(resposta =>resposta.json())
+        .then(data => {
+          log.value = data.log;
+          bairro.value = data.bairro;
+          cid.value = data.cid;
+          est.value = data.uf;
+ 
+      
+
+
    // alert(nome.value + " " + "no botão!!!");
+
+
    saida.innerText = "Nome: "+" "+ nome.value+
           "\n E-mail: " + " " + email.value +
           "\n Telefone" + " " + telefone.value +
-          "\n CEP" + " " + cep.value
-          "\n Logradouro" + "  " + log.value
-          "\n Numero" + " " + num.value
-          "\n Complemento" + " " + comp.value
-          "\n Bairro" + " " + bairro.value
-          "\n Cidade" + " " + cid.value
+          "\n CEP" + " " + cep.value +
+          "\n Logradouro" + "  " + log.value +
+          "\n Numero" + " " + num.value +
+          "\n Complemento" + " " + comp.value +
+          "\n Bairro" + " " + bairro.value +
+          "\n Cidade" + " " + cid.value +
           "\n Estado" + " " + est.value;
+
+         })
+         .catch(error=>alert(error))
 
 }
